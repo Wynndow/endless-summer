707bf5ee9cea1d47240546857adb53d0b8be7c4e
Chris Wynne <chris.wynndow@gmail.com>
2015-12-04 11:21:56 +0000

    Moar refactor

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -2,15 +2,15 @@ class String
 
   def to_wavecase
     output = ''
-    @upper = false
+    upper = false
     self.chars.each do |char|
       if char == ' '
         output << char
       elsif char.class == Fixnum
         output << char
       else
-        output << (@upper ? char.upcase : char.downcase)
-        @upper = !@upper
+        output << (upper ? char.upcase : char.downcase)
+        upper = !upper
       end
     end
   output
