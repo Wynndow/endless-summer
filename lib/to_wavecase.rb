9dc5786eb0a5a73e1f83c763f4534d35ea21cc6c
Chris Wynne <chris.wynndow@gmail.com>
2015-12-04 11:14:16 +0000

    Solution

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,4 +1,29 @@
 class String
 
+  def to_wavecase
+    output = ''
+    @which = :lower
+
+    self.chars.each do |char, i|
+
+      if char == ' '
+        output << char
+      elsif char.class == Fixnum
+        output << char
+      else
+
+          if @which == :lower
+            output << char.downcase
+            @which = :upper
+          elsif @which == :upper
+            output << char.upcase
+            @which = :lower
+          end
+
+      end
+
+    end
+output
+  end
 
 end
